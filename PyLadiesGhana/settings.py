ef7144dec37c27d39587996d4694e0d2dbc71280
Williano <paawilly17@gmail.com>
2018-05-25 11:51:20 +1200

    Pyladies

--- PyLadiesGhana/settings.py
+++ PyLadiesGhana/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+#    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_ROOT = PROJECT_DIR.parent.parent.child('static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (PROJECT_DIR.child('static'),)
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),
+    )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Media file (User images upload)
--- PyLadiesGhana/urls.py
+++ PyLadiesGhana/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
@@ -28,3 +30,6 @@ urlpatterns = [
     path('locations', include('locations.urls', namespace='locations')),
     path('contact', include('contact.urls', namespace='contact'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
