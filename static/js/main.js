847a5aeb4462c8039b9faf4acd3cdda3104c9744
Williano <paawilly17@gmail.com>
2018-06-21 17:08:57 +1200

    UI update

--- static/js/main.js
+++ static/js/main.js
@@ -1,93 +1,126 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
 (function($) {
-	"use strict"
 
-	///////////////////////////
-	// Preloader
-	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
-	});
+	"use strict";
 
-	///////////////////////////
-	// Scrollspy
-	$('body').scrollspy({
-		target: '#nav',
-		offset: $(window).height() / 2
-	});
+	/* --------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
 
-	///////////////////////////
-	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+  	})
+
+
+  	/* --------------------------------------------------- */
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()  
+
+
+  	/*---------------------------------------------------- */
+  	/* FitText Settings
+  	------------------------------------------------------ */  
+  	setTimeout(function() {
+
+   	$('.main-content h1').fitText(.8, { minFontSize: '42px', maxFontSize: '94px' });
+
+  	}, 100);
+
+
+  	/* --------------------------------------------------- */
+	/* lettering js
+	------------------------------------------------------ */
+	$(".kern-this").lettering(); 
+
+
+	/* --------------------------------------------------- */
+  	/* Menu
+   ------------------------------------------------------ */  
+   var toggleButton = $('.menu-toggle'),
+       nav = $('#menu-nav-wrap'),
+       mainContent = $('#main-404-content'),
+       mainHeader = $('.main-header');
+
+	// open-close menu by clicking on the menu icon
+	toggleButton.on('click', function(e){
+
 		e.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 600);
+
+		toggleButton.toggleClass('is-clicked');
+		nav.toggleClass('menu-is-open');
+		mainHeader.toggleClass('menu-is-open');
+		mainContent.toggleClass('menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			// firefox transitions break when parent overflow is changed, 
+			// so we need to wait for the end of the trasition to give the body an overflow hidden
+			$('body').toggleClass('overflow-hidden');
+		});
+			
+		// check if transitions are not supported 
+		if($('html').hasClass('no-csstransitions')) {
+			$('body').toggleClass('overflow-hidden');
+		}
+
 	});
 
-	$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 600);
-	});
+	// close menu clicking outside the menu itself
+	mainContent.on('click', function(e){
 
-	///////////////////////////
-	// Btn nav collapse
-	$('#nav .nav-collapse').on('click', function() {
-		$('#nav').toggleClass('open');
-	});
+		if( !$(e.target).is('.menu-toggle, .menu-toggle span') ) {
 
-	///////////////////////////
-	// Mobile dropdown
-	$('.has-dropdown a').on('click', function() {
-		$(this).parent().toggleClass('open-drop');
-	});
-
-	///////////////////////////
-	// On Scroll
-	$(window).on('scroll', function() {
-		var wScroll = $(this).scrollTop();
-
-		// Fixed nav
-		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
-
-		// Back To Top Appear
-		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
-	});
-
-	///////////////////////////
-	// magnificPopup
-	$('.work').magnificPopup({
-		delegate: '.lightbox',
-		type: 'image'
-	});
-
-	///////////////////////////
-	// Owl Carousel
-	$('#about-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
-		animateOut: 'fadeOut'
-	});
-
-	$('#testimonial-slider').owlCarousel({
-		loop:true,
-		margin:15,
-		dots : true,
-		nav: false,
-		autoplay : true,
-		responsive:{
-			0: {
-				items:1
-			},
-			992:{
-				items:2
+			toggleButton.removeClass('is-clicked');
+			nav.removeClass('menu-is-open');
+			mainHeader.removeClass('menu-is-open');
+			mainContent.removeClass('menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('body').removeClass('overflow-hidden');
+			});
+			
+			// check if transitions are not supported
+			if($('html').hasClass('no-csstransitions')) {
+				$('body').removeClass('overflow-hidden');
 			}
+
 		}
 	});
+
+
+   /* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+	$(".main-content-slides").vegas({
+		transition: 'fade',
+		transitionDuration: 2500,
+		delay: 5000,
+    	slides: [
+       	{ src: "static/img/slides/woods.jpg" },
+        	{ src: "static/img/slides/greens.jpg" },
+        	{ src: "static/imag/slides/dandelion.jpg" }
+    	]
+	});
+
+
+   /* --------------------------------------------------- */
+	/*  Particle JS
+	------------------------------------------------------ */
+	$('.main-content-particle-js').particleground({
+	    dotColor: '#fff',
+	    lineColor: '#555555',
+	    particleRadius: 6,
+	    curveLines: true,
+	    density: 9000,
+	    proximity: 100
+	});  	
+ 
 
 })(jQuery);
